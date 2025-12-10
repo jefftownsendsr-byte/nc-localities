658fb10d7720eef0b4e7631ffdb3ab2caaf03e3f
Jeff <jeff.townsend.sr@gmail.com>
2025-12-10 09:36:02 -0500

    Fix: API Key Typo and Service Worker Cache

--- docs/config.js
+++ docs/config.js
@@ -2,7 +2,7 @@
 // This allows the app to connect to the "Brain" (Backend)
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDvvWfmJRK06bgNfVEaq8cepSwVsC3Tc8Ng",
+    apiKey: "AIzaSyDvWfmJRK06bgNfVEaq8cepSwVsC3Tc8Ng",
     authDomain: "nc-geomapper.firebaseapp.com",
     projectId: "nc-geomapper",
     storageBucket: "nc-geomapper.firebasestorage.app",
--- docs/sw.js
+++ docs/sw.js
@@ -19,6 +19,7 @@ const ASSETS_TO_CACHE = [
 
 // Install Event - Cache Core Assets
 self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Force new SW to enter 'activate' state immediately
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             console.log('[Service Worker] Caching Assets');
@@ -30,13 +31,16 @@ self.addEventListener('install', (event) => {
 // Activate Event - Cleanup Old Caches
 self.addEventListener('activate', (event) => {
     event.waitUntil(
-        caches.keys().then((keyList) => {
-            return Promise.all(keyList.map((key) => {
-                if (key !== CACHE_NAME) {
-                    return caches.delete(key);
-                }
-            }));
-        })
+        Promise.all([
+            clients.claim(), // Force new SW to control all clients immediately
+            caches.keys().then((keyList) => {
+                return Promise.all(keyList.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                }));
+            })
+        ])
     );
 });
 
